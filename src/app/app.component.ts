dfbb4a58b45ade416cadfe20914ab41f7056577b
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-01-30 00:54:38 +0530

    2_2_COMPONENT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import {COURSES} from '../db-data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+
+  maincourse = COURSES[0];
+  rxjscourse = COURSES[1];
+  ngrxcourse = COURSES[2];
+  data = {
+     title : 'angular course'
+    // title : '<h1>angular content</h1><script>alert("Alert")</script>'
+  };
+
+
+  onLogoClick() {
+    alert('Hello World');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  }
 }
