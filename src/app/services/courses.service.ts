2e7db1480ec50db1d2b8aded57bcb8353540f110
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-02 11:01:29 +0530

    13_2_DATA

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {Course} from '../model/course';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +21,9 @@ export class CoursesService {
   }
 
   saveCourse(course: Course) {
-    return this.http.put(`/api/courses/${course.id}`,course).subscribe(
+
+    const headers = new HttpHeaders().set('X-Auth', 'userId');
+    return this.http.put(`/api/courses/${course.id}`, course, {headers}).subscribe(
       () => console.log('save successful')
     );
   }
